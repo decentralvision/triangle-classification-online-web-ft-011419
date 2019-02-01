ca106c2ebf2f4a88909db61807fe84dd6092998a
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-02-01 10:54:45 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,7 +2,7 @@ class Triangle
   attr_accessor :sides
   def initialize(side_1, side_2, side_3)
     @sides = [side_1, side_2, side_3]
-    if sides.any? {|side| side <= 0 || @sides - [side]}
+    if sides.any? {|side| side <= 0 || (@sides - [side]).inject(0, :+) <= side }
   end
   class TriangleError < StandardError
 
