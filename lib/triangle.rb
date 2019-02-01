3bf1370f1145359b47e04e63d299a1be967fdee2
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-02-01 11:06:56 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,7 @@
 class Triangle
   attr_accessor :sides, :kind
   def initialize(side_1, side_2, side_3)
-    @sides = [side_1, side_2, side_3].collect {|side| side.to_f}
+    @sides = [side_1, side_2, side_3]
     if sides.any? {|side| side <= 0 || (@sides - [side]).inject(0, :+) <= side }
       begin
         raise TriangleError
@@ -21,5 +21,4 @@ class Triangle
       "Invalid triangle, each side must be larger than 0 and the length of any side must not exceed the sum of the lengths of the other two sides"
     end
   end
-  self
 end
