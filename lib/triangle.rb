befc8f072a200ca42de7cf15aa0dd177b8fc0ac7
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-02-01 11:34:14 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,28 +1,22 @@
 require 'pry'
 class Triangle
-  attr_accessor :sides
-
+  attr_accessor :sides, :kind
   def initialize(side_1, side_2, side_3)
     @sides = [side_1, side_2, side_3]
-  end
-
-  def kind
-    if @sides.any? {|side| side <= 0 || (@sides).reduce(:+) - side <= side }
+    if sides.any? {|side| side <= 0 || (@sides).reduce(:+) - side <= side }
       begin
-        binding.pry
         raise TriangleError
       rescue TriangleError => error
         puts error.message
       end
     elsif @sides.uniq.size == 1
-      :equilateral
+      @kind = :equilateral
     elsif @sides.uniq.size == 2
-      :isosceles
+      @kind = :isosceles
     elsif @sides.uniq.size == 3
-      :scalene
+      @kind = :scalene
     end
   end
-
   class TriangleError < StandardError
     def message
       "Invalid triangle, each side must be larger than 0 and the length of any side must not exceed the sum of the lengths of the other two sides"
