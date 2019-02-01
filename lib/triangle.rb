a4c360cfff2d813cabe86d86b3c26cf6b0fb1115
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-02-01 11:37:33 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,6 +3,8 @@ class Triangle
   attr_accessor :sides, :kind
   def initialize(side_1, side_2, side_3)
     @sides = [side_1, side_2, side_3]
+  end
+  def kind
     if sides.any? {|side| side <= 0 || (@sides).reduce(:+) - side <= side }
       begin
         raise TriangleError
