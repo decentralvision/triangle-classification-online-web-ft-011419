ee7a7751b67bc75ea2ee6eb823bf77387d557988
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-02-01 11:32:16 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,11 +1,11 @@
 require 'pry'
 class Triangle
   attr_accessor :sides
-  
+
   def initialize(side_1, side_2, side_3)
     @sides = [side_1, side_2, side_3]
   end
-  
+
   def kind
     if @sides.any? {|side| side <= 0 || (@sides).reduce(:+) - side <= side }
       begin
@@ -21,7 +21,7 @@ class Triangle
       :scalene
     end
   end
-  
+
   class TriangleError < StandardError
     def message
       "Invalid triangle, each side must be larger than 0 and the length of any side must not exceed the sum of the lengths of the other two sides"
