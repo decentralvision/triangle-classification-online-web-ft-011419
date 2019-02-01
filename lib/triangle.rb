4f7cbacd2ada8431b407657233424ea6472ff298
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-02-01 11:28:24 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -4,6 +4,7 @@ class Triangle
   def initialize(side_1, side_2, side_3)
     @sides = [side_1, side_2, side_3]
     if sides.any? {|side| side <= 0 || (@sides).reduce(:+) - side <= side }
+      binding.pry
       begin
         raise TriangleError
       rescue TriangleError => error
